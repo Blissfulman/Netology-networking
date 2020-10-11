8d333540263bf763f4738ebb1b474e3afb83b2a7
Evgeny <greatly01@ya.ru>
2020-10-11 23:41:44 +0500

    Empty project

---
+++ GitHub/GitHub/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GitHub
+//
+//  Created by User on 11.10.2020.
+//  Copyright © 2020 Evgeny. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ GitHub/GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by User on 11.10.2020.
+//  Copyright © 2020 Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
