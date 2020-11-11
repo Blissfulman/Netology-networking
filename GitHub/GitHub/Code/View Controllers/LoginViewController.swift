aea9f3c389300cc86732c53f4143254fd3a4492d
Evgeny Novgorodov <blissfulman01@gmail.com>
2020-11-11 13:13:15 +0500

    Local authentication was added

--- GitHub/GitHub/Code/View Controllers/LoginViewController.swift
+++ GitHub/GitHub/Code/View Controllers/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Kingfisher
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
 
@@ -68,6 +69,7 @@ class LoginViewController: UIViewController {
         loginButton.addTarget(self,
                               action: #selector(loginButtonPressed),
                               for: .touchUpInside)
+        authenticateUser()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -185,4 +187,61 @@ extension LoginViewController: UITextFieldDelegate {
         }
         return true
     }
+}
+
+// MARK: - Local authentication
+extension LoginViewController {
+    
+    func authenticateUser() {
+        
+        guard #available(iOS 8.0, *, *) else {
+            print("Версия iOS не поддерживает TouchID")
+            return
+        }
+        
+        let authenticationContext = LAContext()
+        setupAuthenticationContext(context: authenticationContext)
+        
+        let reason = "Fast and safe authentication in your app"
+        var authError: NSError?
+        
+        if authenticationContext.canEvaluatePolicy(
+            .deviceOwnerAuthenticationWithBiometrics, error: &authError
+        ) {
+            authenticationContext.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: reason
+            ) { [unowned self] success, evaluateError in
+                if success {
+                    // Пользователь успешно прошел аутентификацию
+                    startMainApplicationFlow()
+                } else {
+                    // Пользователь не прошел аутентификацию
+                    if let error = evaluateError {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        } else {
+            // Не удалось выполнить проверку на использование биометрических данных или пароля для аутентификации
+            if let error = authError {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+    func startMainApplicationFlow() {
+        print("Success")
+        print("Main application flow started")
+    }
+    
+    func setupAuthenticationContext(context: LAContext) {
+        context.localizedReason = "Use for fast and safe authentication in your app"
+        context.localizedCancelTitle = "Cancel"
+        context.localizedFallbackTitle = "Enter password"
+        
+        context.touchIDAuthenticationAllowableReuseDuration = 600
+    }
+
 }
